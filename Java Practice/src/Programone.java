a02c3d2666e95196a9b9b4411bd055fe5babbc08
ngire <ngire@Gireesh-miBook>
2022-07-24 11:15:11 +0530

    java new code

---
+++ Java Practice/src/Programone.java
@@ -0,0 +1,34 @@
+
+public class Programone
+{
+
+	public static void main(String[] args)
+	{
+		
+	 System.out.println("the first program after installiation of seleniumj"+"\n");
+
+
+		
+      String s1="Gireesh";
+      String s2="";
+     char ch; 
+     
+      
+      char c1=s1.charAt(0);
+      
+      System.out.println(c1);
+
+      for(int i=0;i<s1.length();i++)
+      {
+    	   ch=s1.charAt(i);
+    	   s2=ch+s1;
+    	  
+    	  
+      }
+      System.out.println(s2);
+      
+      
+      
+      
+	}
+}
